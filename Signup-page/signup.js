1fa0494c499562980d53da44993cd80e3f74110f
anilghaswan <ghaswananil56@gmail.com>
2026-03-17 10:01:00 +0530

    add

---
+++ Signup-page/signup.js
@@ -0,0 +1,11 @@
+let form = document.getElementById("sign-up-form");
+let name = document.getElementById(" Name");
+let email = document.getElementById("E-mail");
+let password = document.getElementById("password");
+
+function sinup() {
+  console.log(
+    "your account deatiles are submit ",
+    alert("your form will be  submit "),
+  );
+}
